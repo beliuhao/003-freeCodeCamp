b262b5b331a2aaabda54578a4913965e86b012ad
beliuhao <contactliuhao@gmai.com>
2018-07-28 14:38:47 +0800

    use the VS Code's prettier plugin to prettify the format of html, css, js files

--- P001/src/js/app.js
+++ P001/src/js/app.js
@@ -1,6 +1,5 @@
 window.onload = init;
-function init(){
-
+function init() {
   let minSalary = document.getElementById("minSalary");
   let maxSalary = document.getElementById("maxSalary");
   let rangeLeft = document.getElementById("rangeLeft");
@@ -11,47 +10,8 @@ function init(){
   // Update the current slider1 value (each time you drag the slider handle)
   minSalary.oninput = function() {
     rangeLeft.value = this.value;
-  }
-    maxSalary.oninput = function() {
+  };
+  maxSalary.oninput = function() {
     rangeRight.value = this.value;
-  }
+  };
 }
-
-// <!doctype html>
-// <html lang="en">
-// <head>
-//   <meta charset="utf-8">
-//   <meta name="viewport" content="width=device-width, initial-scale=1">
-//   <title>jQuery UI Slider - Range slider</title>
-//   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-//   <link rel="stylesheet" href="/resources/demos/style.css">
-//   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-//   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-//   <script>
-//   $( function() {
-//     $( "#slider-range" ).slider({
-//       range: true,
-//       min: 0,
-//       max: 500,
-//       values: [ 75, 300 ],
-//       slide: function( event, ui ) {
-//         $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-//       }
-//     });
-//     $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-//       " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-//   } );
-//   </script>
-// </head>
-// <body>
-
-// <p>
-//   <label for="amount">Price range:</label>
-//   <input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
-// </p>
-
-// <div id="slider-range"></div>
-
-
-// </body>
-// </html>
