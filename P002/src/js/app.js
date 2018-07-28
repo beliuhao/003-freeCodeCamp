70388fef42bf373e3f96963fd604938a449efc72
beliuhao <contactliuhao@gmai.com>
2018-07-28 16:31:25 +0800

    use jquery instead of the Vanilla js aka plain js

--- P002/src/js/app.js
+++ P002/src/js/app.js
@@ -1,13 +1,22 @@
-window.onload = init;
-function init() {
-  let navbarLink = document.querySelectorAll("#navbarCollapse .navbar-nav a");
-  let navbarLi = document.querySelectorAll("#navbarCollapse .navbar-nav li");
-  navbarLink.forEach(a => {
-    a.addEventListener("click", function() {
-      navbarLi.forEach(li => {
-        li.classList.remove("active");
-      })
-      this.parentNode.classList.add("active");
-    })
-  })
-}
+// window.onload = init;
+// function init() {
+//   let navbarLink = document.querySelectorAll("#navbarCollapse .navbar-nav a");
+//   let navbarLi = document.querySelectorAll("#navbarCollapse .navbar-nav li");
+//   navbarLink.forEach(a => {
+//     a.addEventListener("click", function() {
+//       navbarLi.forEach(li => {
+//         li.classList.remove("active");
+//       })
+//       this.parentNode.classList.add("active");
+//     })
+//   })
+// }
+
+$("document").ready(
+  function init() {
+    $( '#navbarCollapse .navbar-nav a' ).on( 'click', function () {
+      $( '#navbarCollapse .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+      $( this ).parent( 'li' ).addClass( 'active' );
+    });
+  }
+)
